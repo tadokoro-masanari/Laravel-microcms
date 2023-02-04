c84c0767aeab95817ca33c3fae94895f5622fb43
田所雅成 <tadokorotadashisei@tadokorotadashiseinoMacBook-Pro.local>
2023-02-05 05:14:31 +0900

    記事の一覧ページを作成しました

---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="container">
+    <p>&copy; 会社名 2017–2023</p>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10/build/highlight.min.js"></script>
+<script>hljs.highlightAll();</script>
+<script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
+<script src="https://bootstrap-guide.com/js/samples.js"></script>
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,23 @@
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="ナビゲーションバーといくつかの基本的なグリッドの列をジャンボトロンの周りに構築。">
+<!-- OGP -->
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@cccabinet">
+	<meta property="og:site_name" content="Bootstrap5設置ガイド" />
+	<meta property="og:title" content="コンテンツ付きジャンボトロンの実例～Bootstrap5設置ガイド" />
+	<meta property="og:url" content="https://bootstrap-guide.com/sample/jumbotron-contents" />
+	<meta property="og:type" content="article" />
+	<meta property="og:description" content="ナビゲーションバーといくつかの基本的なグリッドの列をジャンボトロンの周りに構築。" />
+	<meta property="fb:app_id" content="2270152383309485" />
+	<meta property="og:image" content="https://bootstrap-guide.com/files/example_files/33/jumbotron-contents.png" />
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="960">
+	<meta property="og:image:height" content="600">
+	<title>コンテンツ付きジャンボトロンの実例～Bootstrap5設置ガイド</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/styles/github.min.css">
+	<link rel="stylesheet" href="https://bootstrap-guide.com/css/samples.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css">
+</head>
